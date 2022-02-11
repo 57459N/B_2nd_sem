6c84f6d4d13baa6cbddfed4a1bfb64e4c8ac08a3
57459N <stasun008@gmail.com>
2022-02-11 10:46:07 +0300

    main

--- B_2/Main.cpp
+++ B_2/Main.cpp
@@ -3,16 +3,19 @@
 
 int m11();
 int m12();
+int m13();
 
-int m14();
+int m15();
 
-int m16();
+int m21();
+
+
+void create_numbers();
 
 using namespace std;
 
 int main()
 {
-	
-	return m16();
-	
+	m21();
+	return 0;
 }
