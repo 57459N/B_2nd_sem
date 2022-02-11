2ecb68eb86a79e6b089691f982f1600890e71c2d
57459N <stasun008@gmail.com>
2022-02-11 12:42:21 +0300

    1.6 to check

--- B_2/B_2/1.6.cpp
+++ B_2/B_2/1.6.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 int strLessOrEqual(char* str1, char* str2) {
-    int n1 = 0;
-    int n2 = 0;
-    while (str1[n1++]);
-    while (str1[n2++]);
+    int l1 = strlen(str1);
+    int l2 = strlen(str2);
 
-    for (int i = 0; i < min(n1, n2); i++) {
+    int n = min(l1, l2);
+    for (int i = 0; i < n; i++) {
         if (str1[i] != str2[i])
             return str1[i] <= str2[i];
     }
-    return n1 <= n2;
+    return l1 <= l2;
 }
 
 template <typename T>
@@ -19,8 +18,8 @@ void merge(T* array, int left, int mid, int right) {
     int lSub = mid - left + 1;
     int rSub = right - mid;
 
-    int* lArray = new int[lSub];
-    int* rArray = new int[rSub];
+    T* lArray = new T[lSub];
+    T* rArray = new T[rSub];
 
     for (int i = 0; i < lSub; i++)
         lArray[i] = array[left + i];
@@ -52,6 +51,9 @@ void merge(T* array, int left, int mid, int right) {
         rIndex++;
         mergedIndex++;
     }
+
+    delete[] lArray;
+    delete[] rArray;
 }
 
 void merge(char** array, int left, int mid, int right) {
@@ -91,24 +93,71 @@ void merge(char** array, int left, int mid, int right) {
         rIndex++;
         mergedIndex++;
     }
+    delete[] lArray;
+    delete[] rArray;
 }
 
 template <typename T>
-void mergeSort(T* array, int begin, int end) {
+void mergeSort(T* arr, int begin, int end) {
     if (begin >= end)
         return;
 
     int mid = begin + (end - begin) / 2;
-    mergeSort(array, begin, mid);
-    mergeSort(array, mid + 1, end);
-    merge(array, begin, mid, end);
+    mergeSort(arr, begin, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, begin, mid, end);
 }
 template <typename T>
-void printArray(T A, int size) {
-    for (auto i = 0; i < size; i++)
-        cout << A[i] << " ";
+void printArray(T* arr, int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
 }
 
+void printArray(char* arr, int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+}
+
+template <typename T>
+void randArr(T* arr, int size) {
+    srand(time(nullptr));
+    for (int i = 0; i < size; i++){
+        arr[i] = rand() % 99 + 1;
+    }
+}
+
+void randArr(double* arr, int size) {
+    srand(time(nullptr));
+    for (int i = 0; i < size; i++) {
+        arr[i] = (rand() % 9999 + 1) / 100.;
+    }
+}
+
+void randArr(char** arr, int size) {
+    srand(time(nullptr));
+    for (int i = 0; i < size; i++) {
+        int len = rand() % 5 + 1;
+        arr[i] = new char[len];
+        for (int j = 0; j < len; j++) {
+            arr[i][j] = rand() % 57 + 33;
+        }
+        arr[i][len] = '\0';
+    }
+}
+template <typename T>
+void task(T arr, int n) {
+    randArr(arr, n);
+    printArray(arr, n);
+    mergeSort(arr, 0, n - 1);
+    cout << endl;
+    printArray(arr, n);
+}
+
+enum CHOICE {
+    DOUBLE = 1,
+    CHAR,
+    STR
+};
 
 int m16() {
     
@@ -116,24 +165,41 @@ int m16() {
     cout << "Enter size of array: ";
     cin >> n;
 
-    srand(time(nullptr));
-    //int* arr = new int[20];
-    char** arr = new char*[n];
-  
-    cin.clear();
-    cin.ignore();
-    for (int i = 0; i < n; i++) {
-        //arr[i] = rand() % 99 + 1;
-        arr[i] = new char[1000];
-        cin.getline(arr[i], 1000);
-    }
+    short choice = 0;
+    cout << "0 - int (default)" << endl
+        << "1 - double" << endl
+        << "2 - char" << endl
+        << "3 - string" << endl;
+    cout << "Choose type of elements: ";
+    cin >> choice;
+
     
-    printArray(arr, n);
-
-    mergeSort(arr, 0, n - 1);
-
-    cout << endl;
-    printArray(arr, n);
+    switch (choice) {
+    case(DOUBLE):{
+        double* arr = new double[n];
+        task(arr, n);
+        delete[] arr;
+        break;
+    }
+    case(CHAR): {
+        char* arr = new char[n];
+        task(arr, n);
+        delete[] arr;
+        break;
+    }
+    case(STR):{
+        char** arr = new char*[n];
+        task(arr, n);
+        delete[] arr;
+        break;
+    }
+    default:{
+        int* arr = new int[n];
+        task(arr, n);
+        delete[] arr;
+        break;
+    }
+    }
 
     return 0;
 }
