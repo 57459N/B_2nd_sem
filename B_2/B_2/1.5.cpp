6f40d88415c73b99a4bb3251f3d9e6ff08c905b9
57459N <stasun008@gmail.com>
2022-02-21 23:36:47 +0300

    1.5 confirmed

--- B_2/B_2/1.5.cpp
+++ B_2/B_2/1.5.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 
-#define e 2.71828182845904523536
 
 using namespace std;
 
+int Integral_counter= 0;
+
 double f(double x, double s, double t) {
-	return t * pow(e, x / 2) / sqrt(x + s);
+	return t * exp( x / 2) / sqrt(x + s);
 }
 
 double Yi(double(*f)(double, double, double), double a, double s, double t, double h, int i) {
 	return f(a + h * i, s, t);
 }
 double Integral(double(*f)(double, double, double), double s, double t, double a, double b, double n) {
-	int h = (b - a) / 2 / n;
+	double h = (b - a) / 2 / n;
 	double I = Yi(f, a, s, t, h, 0) + Yi(f, a, s, t, h, 2 * n);
 	
 	for (int i = 2; i < 2 * n; i += 2) {
@@ -23,18 +25,19 @@ double Integral(double(*f)(double, double, double), double s, double t, double a
 		I += 4 * Yi(f, a, s, t, h, i);
 	}
 	I *= h / 3;
+	Integral_counter++;
 	return I;
 }
 
 double doubleAccuracy(double(*f)(double, double, double), double s, double t, double a, double b, double E) {
 	int n = 4;
-	int S1 = Integral(f,s,t,a,b,n);
-	int S2 = 0;
+	double S1 = Integral(f,s,t,a,b,n);
+	double S2 = 0;
 	do {
 		S2 = S1;
 		n *= 2;
 		S1 = Integral(f, s, t, a, b, n);
-		cout << "S1: " << S1 << "  S2: " << S2 << endl;
+		//cout << "S1: " << S1 << "  S2: " << S2 << endl;
 	} while (abs(S1 - S2) > E);
 	
 	return S1;
@@ -42,11 +45,14 @@ double doubleAccuracy(double(*f)(double, double, double), double s, double t, do
 
 int m15() {
 
-	double s = 1;
-	double t = 0.5;
 	double a = 0;
 	double b = 1.047;
-
-	cout << doubleAccuracy(f, s, t, a, b, 1e-6);
+	cout << setw(2) << "S" << setw(6) << "T" << setw(9) << "Int" << setw(7) <<  "K" << endl;
+	for (double s = 0.5; s <= 2; s += 0.5) {
+		for (double t = 0.5; t <= 4; t += 0.5) {
+			cout << setw(3) << s << "  " << setw(4) << t << "  " << setw(8) << doubleAccuracy(f, s, t, a, b, 1e-6) << setw(5) << Integral_counter << endl;
+		}
+	}
+	
 	return 0;
 }
