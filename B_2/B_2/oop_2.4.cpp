883b3258335423f2ff9a4516cfde25a6e9040b6c
57459N <stasun008@gmail.com>
2022-03-30 11:09:58 +0300

    2.4 confirmed 3.2 confirmed 2.5 confirmed

--- B_2/B_2/oop_2.4.cpp
+++ B_2/B_2/oop_2.4.cpp
@@ -6,17 +6,33 @@ using namespace std;
 
 class Date {
 private:
+	/*
+		Days since 1.1.0001
+	*/
 	int days = 0;
-	static const int months[];
+	static constexpr int months[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	
 public:
 
 	Date() = default;
 	Date(const char* d) {
-		days = str_to_days(d);
+		this->days = str_to_days(d);
 	}
 	Date(const Date& d) {
 		this->days = get_days();
+	}
+
+	Date(const int d) {
+		this->days = d;
+	}
+	Date& operator= (const int d) {
+		this->days = d;
+		return *this;
+	}
+	Date& operator= (const char* dstr) {
+		int d = Date::str_to_days(dstr);
+		this->days = d;
+		return *this;
 	}
 
 	~Date() {
@@ -24,7 +40,7 @@ public:
 	}
 
 	static bool is_leap(const int& year)  {
-		return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0);
+		return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
 	}
 
 	static char* days_to_str(int days_amount) {
@@ -41,7 +57,7 @@ public:
 			month++;
 		}
 		days_amount += is_leap(year);
-		day += days_amount-2;
+		day += days_amount;
 
 		char* str = new char[11]{};
 
@@ -49,7 +65,8 @@ public:
 		return str;
 	}
 
-	int str_to_days(const char* str) {
+	static int str_to_days(const char* str) {
+		int d = 0;
 		stringstream ss(str);
 		string D[] = {"","",""};
 
@@ -57,35 +74,68 @@ public:
 			getline(ss, D[i], '.');
 		}
 
-		days += stoi(D[0]);
+		d += stoi(D[0]);
 		for (int i = 0; i < stoi(D[1]); i++) {
-			days += months[i];
+			d += months[i];
 		}
-		days += is_leap(stoi(D[2]));
+		d += is_leap(stoi(D[2]));
 
 		for (int i = 0; i < stoi(D[2]); i++) {
-			days += 365 + is_leap(i);
+			d += 365 + is_leap(i);
 		}
-		return days;
+		return d;
 	}
+
+	int get_days() const {
+		return this->days;
+	}
+
+	int days_between(const Date& d) {
+		return this->days - d.get_days();
+	}
+
+	Date& operator- (const int days_amount) {
+		this->days -= days_amount;
+		return *this;
+	}
+	
+	Date& operator+ (const int days_amount) {
+		this->days += days_amount;
+		return *this;
+	}
+
+	
+	friend std::istream& operator>> (std::istream& str, Date& date) {
+		char* d = new char[20];
+		str.getline(d, 20);
+		int day = date.str_to_days(d);
+		date.days = day;
+		return str;
+	}	
 
 	friend std::ostream& operator<< (std::ostream& str, const Date& date) {
 		int days_amount = date.get_days();
 		str << Date::days_to_str(days_amount);
 		return str;
-	}
-
-	int get_days() const{
-		return this->days;
-	}
+	}	
 };
 
-const int Date::months[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 
 int oop24() {
-	Date date = Date("12.2.2004");
+	char str[] = "4.1.3020";
+	Date date1 = str;
+	cout << date1 << endl;
 
-	cout << date;
+	cout << Date::str_to_days(str) << endl;
+	Date date2 = Date::str_to_days(str) + 10;
+	cout << date2 << endl;
 
+	Date date3;
+	stringstream ss(str);
+	ss >> date3;
+	cout << date3 << endl;
+
+	cout << date2.days_between(date3);
 	return 0;
 }
--- B_2/B_2/oop_3.2.cpp
+++ B_2/B_2/oop_3.2.cpp
@@ -36,8 +36,6 @@ public:
             return *this;
         }
 
-
-
         friend std::ostream& operator<< (std::ostream& str, const Complex_oop& o) {
             if (o.im >= 0) {
                 str << setw(25) << o.re << "+" << o.im << "i";
@@ -51,11 +49,13 @@ public:
 
 private:    
     vector<vector<Complex_Matrix::Complex_oop>> arr;
-    int width;
-    int heigth;
+    int w = 3;
+    int h = 3;
 
+public:
 
-    Complex_Matrix(const int& n, const int& m) {
+    Complex_Matrix() = default;
+    Complex_Matrix(const int n, const int m) {
         Complex_oop temp_num(0, 0);
         vector<Complex_oop> temp_vec(m);
         for (int i = 0; i < m; i++) {
@@ -64,28 +64,27 @@ private:
         for (int i = 0; i < n; i++) {
             this->arr.push_back(temp_vec);
         }
-        heigth = n;
-        width = m;
+        h = n;
+        w = m;
     }
     ~Complex_Matrix() {
-        heigth = 0;
-        width = 0;
+        h = 0;
+        w = 0;
         arr.clear();
     }
 
-public:
-    int heigth() {
-        return this->heigth;
+    int heigth() const {
+        return this->h;
     }
 
-    int width() {
-        return this->width;
+    int width() const {
+        return this->w;
     }
 
     void square() {
 
-        int n = this->heigth;
-        int m = this->width;
+        int n = this->h;
+        int m = this->w;
         if(n==m){
             Complex_Matrix temp_m(n, m);
 
@@ -96,21 +95,35 @@ public:
                     Complex_oop sum(0, 0);
                     for (int l = 0; l < n; l++)
                     {
-                        sum += [i][l] * arrB[l][j];
+                        sum += this->arr[i][l] * this->arr[l][j];
                     }
-                    arrC[i][j] = sum;
+                    temp_m.arr[i][j] = sum;
                 }
             }
+            *this = temp_m;
         }
         else {
             cout << "Can't square not square matrix" << endl;
-        }
-        
+        }        
     }
+
+    friend std::ostream& operator<< (std::ostream& str, const Complex_Matrix& o) {
+        for (int i = 0; i < o.heigth(); i++) {
+            for (int j = 0; j < o.width(); j++) {
+                cout << o[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        return str;
+    }
+
+    const vector<Complex_oop>& operator[] (int idx) const { return this->arr[idx]; }
+
+    vector<Complex_oop>& operator[] (int idx) { return this->arr[idx]; }
 
 
 };
-
 
 
 void fillV(vector<int> &v, string path) {
@@ -132,19 +145,30 @@ void do_21() {
 	fillV(vec, "numbers.txt");
 
 	int s = vec.size();
-	for (int i = 0; i < s; i++) {
+	for (int i = s; i < 0; i++) {
 		if (vec[i]) {
 			cout << i << " ";
 		}
-
 	}
 }
 
-
+void do_22() {
+    Complex_Matrix Mat(3, 3);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            Mat[i][j] = Complex_Matrix::Complex_oop(1.2, 1.1);
+        }
+    }
+    Mat.square();
+    Mat.square();
+    
+    cout << Mat << endl;;
+}
 
 int oop32() {
 
-	
+    //do_21();
+    do_22();
 
 	return 0;
 }
